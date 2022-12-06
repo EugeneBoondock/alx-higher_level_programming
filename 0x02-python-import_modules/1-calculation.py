c18280b80f985957294d90ba035150b2b5f2dca2
EugeneBoondock <philosncube@gmail.com>
2022-12-06 09:49:47 +0000

    fix

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-from calculator_1 import add
+from calculator_1 import add, sub, mul, div
 
 if __name__ == "__main__":
 
@@ -10,35 +10,11 @@ if __name__ == "__main__":
     result = add(a, b)
     print("{} + {} = {}".format(a, b, result))
 
-
-from calculator_1 import sub
-
-if __name__ == "__main__":
-
-    a = 10
-    b = 5
-
     result = sub(a, b)
     print("{} - {} = {}".format(a, b, result))
 
-
-from calculator_1 import mul
-
-if __name__ == "__main__":
-
-    a = 10
-    b = 5
-
     result = mul(a, b)
     print("{} * {} = {}".format(a, b, result))
-
-
-from calculator_1 import div
-
-if __name__ == "__main__":
-
-    a = 10
-    b = 5
 
     result = div(a, b)
     print("{} / {} = {}".format(a, b, result))
