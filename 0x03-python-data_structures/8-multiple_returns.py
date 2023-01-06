454438a106aff24fe73f5e33277944e6ae2cabc3
EugeneBoondock <philosncube@gmail.com>
2023-01-06 17:53:05 +0000

    fix

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
-    return (sentence[0] if sentence else None, int(len(sentence)))
+    return (sentence[0] if sentence != "" else None, len(sentence))
