f461bcf75c3e3c6019270893e80e983bae6a57ef
EugeneBoondock <philosncube@gmail.com>
2023-01-16 19:23:39 +0000

    fix

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 
-def print_dict_by_keys(a_dictionary):
+def print_sorted_dictionary(a_dictionary):
     for key, value in sorted(a_dictionary.items(), key=lambda item: item[0]):
         print(key, ":", value)
