2191a8c6e1f54571550380fc7f367a54b0840cbf
EugeneBoondock <philosncube@gmail.com>
2023-01-16 19:50:31 +0000

    fix

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -2,7 +2,10 @@
 
 
 def update_dictionary(a_dictionary, key, value):
-    if isinstance(key, str):
-        a_dictionary.update({key: value})
+    if key not in a_dictionary:
+        a_dictionary[key] = value
     else:
-        raise ValueError("Key must be a string")
+        for i in a_dictionary:
+            if i == key:
+                a_dictionary[i] = value
+    return a_dictionary
